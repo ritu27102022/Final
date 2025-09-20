690db65b6c6806991a82409a2124542ced964ad9
LENOVO <LENOVO@DESKTOP-NMF9K3U>
2025-09-20 16:01:27 +0530

    final project of Git

---
+++ FinalGitPractice/src/com/cjc/A.java
@@ -0,0 +1,22 @@
+package com.cjc;
+
+public class A 
+{
+ 
+	
+		public void a1()
+		{
+			System.out.println("This is a1 method");
+		}
+		
+		public void a2()
+		{
+			System.out.println("This is a2 methods");
+		}
+		public void a3()
+		{
+			System.out.println("This is a3");
+		}
+	
+
+}
